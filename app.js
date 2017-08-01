2a29370fb4a314b4bb3fec9cad5a1b55fec501ec
Yana Wilkins <yav9774@gmail.com>
2017-08-01 14:50:35 -0400

    done

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 const express = require('express')
 const app = express()
-
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
 
-const todos = ['Wash the car']
-console.log(todos)
+const jsonfile = require('jsonfile')
 
 app.engine('mst', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mst')
 
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+//from package website
+let data = jsonfile.readFileSync('./todolist.json')
+
+let todos = data.todos
+let done = data.done
 
 app.get('/', function(req, res) {
-  res.render('home', { todos: todos })
+  res.render('home', { todos, done })
 })
 
 app.post('/', function(req, res) {
-  todos.push(req.body.todo)
+  todos.push(req.body.addHere)
+  jsonfile.writeFile('./todolist.json', { todos, done }) //stores data on the server forever!
+  res.redirect('/')
+})
+
+app.post('/completed', function(req, res) {
+  // trying to push "const done" to the ul
+  done.push(req.body.done)
+  // want remove from todos
+
+  todos = todos.filter(task => {
+    return task !== req.body.done
+  })
+
+  jsonfile.writeFile('./todolist.json', { todos, done })
+  //const indexOfItem = todos.indexOf(done)
+
   res.redirect('/')
 })
 
